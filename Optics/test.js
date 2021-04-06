0f49fa9cbc3357d02b87be64301d80a49fbeb277
Amarachi Amaechi <31285686+amycruz97@users.noreply.github.com>
2021-04-06 14:43:25 +0100

    Add files via upload

---
+++ Optics/test.js
@@ -0,0 +1,35 @@
+npm init -y
+
+
+______[ROOT FOLDER]
+  |
+  |___article.controller.js
+  |
+  |___Article.js
+  |
+  |___articleService.js
+  |
+  |___server.js
+  |
+  |___article.routes.js
+  |
+  
+
+  {
+    "name": "tdd-with-nodejs",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "keywords": [],
+    "author": "",
+    "license": "ISC"
+  }
+
+  node ServiceWorkerRegistration.js
+  npm i --save-dev express nodemon mongoose body-parser dotenv 
+
+
+  mongoURI = mongodb+srv://ugwustanley:unn247790@cluster0.k7fce.mongodb.net/Article
